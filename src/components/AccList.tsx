7235904959abd007607eacc69b830293d338dcf0
0mkar <0mkar@protonmail.com>
2021-06-04 03:25:07 +0530

    Add account copy

--- src/components/AccList.tsx
+++ src/components/AccList.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { ToastAndroid } from 'react-native';
 import { List } from 'react-native-paper';
+import Clipboard from '@react-native-clipboard/clipboard';
 import { listAccounts, setGethAccount } from '../utils/sign';
 import { IAccount } from '../utils/types';
 import { AppContext } from '../utils/appContext';
 
 export const AccountsList = () => {
-  const [account, setAccount] = useState<IAccount>();
   const [expanded, setExpanded] = useState(false);
   const [error, setError] = useState<Error>();
-  const { accounts, setAccounts } = useContext(AppContext);
+  const { account, setAccount, accounts, setAccounts } = useContext(AppContext);
   const handlePress = () => setExpanded(!expanded);
   useEffect(() => {
     ToastAndroid.showWithGravity(
@@ -27,14 +27,24 @@ export const AccountsList = () => {
       .catch(err => {
         setError(err);
       });
-  }, [setAccounts]);
+  }, [setAccounts, setAccount]);
+  const copyAddress = () => {
+    console.log('Left button pressed.');
+    Clipboard.setString(account?.address ? account.address : '');
+    ToastAndroid.showWithGravity(
+      'Public address copied to clipboard!',
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM,
+    );
+  };
   return (
     <List.Section title="Public key">
       <List.Accordion
         title={account?.address}
-        left={props => <List.Icon {...props} icon="folder" />}
+        left={props => <List.Icon {...props} icon="content-copy" />}
         expanded={expanded}
-        onPress={handlePress}>
+        onPress={handlePress}
+        onLongPress={copyAddress}>
         {accounts.map(acc => (
           <List.Item
             title={acc.address}
